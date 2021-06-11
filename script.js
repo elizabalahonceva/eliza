6202047aefd24cfff465eed2f9f06e4b8bfc56b1
Liza <eliza@mail.ru>
2021-06-11 17:39:41 +0300

    21 lesson

--- script.js
+++ script.js
@@ -76,73 +76,73 @@ P.S. Функции вызывать не обязательно*/
 
 'use strict';
 
-let numberOfFilms;
+// let numberOfFilms;
 
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+// function start() {
+//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
-    }
-}
+//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+//     }
+// }
 
-start();
+// start();
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?');
-        const b = prompt('На сколько оцените его?');
+// function rememberMyFilms() {
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt('Один из последних просмотренных фильмов?');
+//         const b = prompt('На сколько оцените его?');
     
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            console.log('done');
-            personalMovieDB.movies[a] = b;
-        } else {
-            console.log('error');
-            i--;
-        }  
-    }
-}
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//             console.log('done');
+//             personalMovieDB.movies[a] = b;
+//         } else {
+//             console.log('error');
+//             i--;
+//         }  
+//     }
+// }
 
-rememberMyFilms();
+// rememberMyFilms();
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >=10 && personalMovieDB.count < 30) {
-        console.log('Вы классический зритель');
-    } else if (personalMovieDB.count >= 30) { 
-        console.log('Вы классический зритель');
-    } else {
-        console.log('Произошла ошибка');
-    }
-}
+// function detectPersonalLevel() {
+//     if (personalMovieDB.count < 10) {
+//         console.log('Просмотрено довольно мало фильмов');
+//     } else if (personalMovieDB.count >=10 && personalMovieDB.count < 30) {
+//         console.log('Вы классический зритель');
+//     } else if (personalMovieDB.count >= 30) { 
+//         console.log('Вы классический зритель');
+//     } else {
+//         console.log('Произошла ошибка');
+//     }
+// }
 
-detectPersonalLevel();
+// detectPersonalLevel();
 
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
+// function showMyDB(hidden) {
+//     if (!hidden) {
+//         console.log(personalMovieDB);
+//     }
+// }
 
-showMyDB();
+// showMyDB();
 
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-        personalMovieDB.genres[i - 1] = genre;
-    }
-}
+// function writeYourGenres() {
+//     for (let i = 1; i <= 3; i++) {
+//         let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+//         personalMovieDB.genres[i - 1] = genre;
+//     }
+// }
 
-writeYourGenres();
+// writeYourGenres();
 
 // Callback - функции -------------------------------------------------------------------------------
 /* function one(name, callback) {
@@ -230,3 +230,33 @@ options.makeTest(); */
 
 const {border, bg} = options.colors;
 console.log(bg); */
+
+// Перебор и вывод элементов массива с помощью for, for of, forEach ----------------------------
+/* const arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+for (let value of arr) {
+    console.log(value);
+}
+
+arr.forEach(function(value, i, add) {
+    console.log(`${i+1} - ${value} из массива ${add}`);
+}); */
+
+// Из одной строки в массив
+/* const str = prompt("", "");
+const products = str.split(", ");
+console.log(products); */
+
+// Из элементов массива в одну строку
+/* const arr = [1, 2, 3, 4];
+const str = arr.join("; ");
+console.log(str); */
+
+// Сортировка элементов массива - тип string по ал
+/* const arr = ['a', 'd', 'c', 'b'];
+arr.sort();
+console.log(arr); */
